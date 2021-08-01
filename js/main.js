845a5fb9e83ba06521278166abe9234c782d667f
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-08-01 19:20:12 +0300

    add delivery fee

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function getCrust() {
+    var selectedCrust = document.getElementById("crust").value;
+    return parseInt(selectedCrust);
+}
+function getToppings() {
+    var selectedToppings = document.getElementById("toppings").value;
+    return parseInt(selectedToppings);
+}
+function getQuantity() {
+    var selectedQuantity = document.getElementById("QTY").value;
+    return parseInt(selectedQuantity);
